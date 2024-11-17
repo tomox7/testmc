e4e89b66acf1dc58c506a73c33df2f6973169111
tabaz <tabaz@example.com>
2024-11-17 14:20:33 +0900

    a

--- install/install.bat
+++ install/install.bat
@@ -6,15 +6,7 @@ set MC_HOME=%AppData%\.minecraft
 echo %MC_HOME%
 explorer %MC_HOME%
 cd /d %~dp0
-set /p INPUTSTR="open minecraft folder ?(y or n & enter)"
 
-IF %INPUTSTR%==y (
-GOTO CP
-) ELSE (
-GOTO END
-)
-
-:CP
 If not exist %MC_HOME%\mods mkdir %MC_HOME%\mods
 attrib -r %MC_HOME%\mods
 ren %MC_HOME%\mods mods_bk_%iddt%
@@ -32,5 +24,5 @@ mkdir %MC_HOME%\shaderpacks_bk_%iddt%
 xcopy /s /e %MC_HOME%\shaderpacks %MC_HOME%\shaderpacks_bk_%iddt%
 copy shaderpacks %MC_HOME%\shaderpacks
 call fabric-installer-1.0.1
-:END
+
 pause
