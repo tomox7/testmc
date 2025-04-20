2872eea9ea4d405aeef0d6558fbd7280dc889faf
atabaz <tabaz@example.com>
2025-04-20 14:17:46 +0900

    a

--- install/install.bat
+++ install/install.bat
@@ -23,6 +23,6 @@ If not exist %MC_HOME%\shaderpacks mkdir %MC_HOME%\shaderpacks
 mkdir %MC_HOME%\shaderpacks_bk_%iddt%
 xcopy /s /e %MC_HOME%\shaderpacks %MC_HOME%\shaderpacks_bk_%iddt%
 copy shaderpacks %MC_HOME%\shaderpacks
-call fabric-installer-1.0.1
+call fabric-installer-1.0.3
 
 pause
